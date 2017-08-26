fbc507b9b5b99a61098e5cbb96c4b544b413d1f6
Siedrix <siedrix@gmail.com>
2017-08-26 08:51:36 -0500

    Added pretty post format

--- models/post.js
+++ models/post.js
@@ -5,13 +5,34 @@ const { v4 } = require('uuid')
 const dataTables = require('mongoose-datatables')
 
 const postSchema = new Schema({
-  created_at: {type: Date, default: Date.now},
+  createdAt: {type: Date, default: Date.now},
   user: { type: Schema.Types.ObjectId, ref: 'User' },
   url: { type: Schema.Types.ObjectId, ref: 'Url' },
   description: String,
   tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
   uuid: { type: String, default: v4 }
 })
+
+postSchema.methods.format = function () {
+  const data = {
+    description: this.description,
+    uuid: this.uuid,
+    createdAt: this.createdAt
+  }
+
+  if (this.url) {
+    data.content = this.url.content
+    data.title = this.url.title
+    data.text = this.url.text
+    data.html = this.url.html
+    data.publishData = this.url.date
+    data.siteName = this.url.siteName
+    data.siteIcon = this.url.icon
+    data.href = this.url.pageUrl
+  }
+
+  return data
+}
 
 postSchema.plugin(dataTables)
 
