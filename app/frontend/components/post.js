cc7436e450938bbb37f154f3d88a7594467c33ab
Siedrix <siedrix@gmail.com>
2017-08-27 11:39:20 -0500

    Added mobile cards

--- app/frontend/components/post.js
+++ app/frontend/components/post.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
-moment.locale('en');
+
+import tree from '~core/tree'
+
+moment.locale('en')
 
 class Post extends Component {
   constructor (props) {
@@ -20,74 +23,69 @@ class Post extends Component {
 
   render () {
     const postsTags = this.props.data.tags.map(t => {
-      return <Link className="level-item" to={"/t/" + t.slug}>
-        <small className="button is-light" >{t.name}</small>
+      return <Link className='' to={'/t/' + t.slug}>
+        <small className='button is-light' style={{margin: 5}}>{t.name}</small>
       </Link>
     })
 
     var editLink
-    if(tree.get("user").uuid === this.props.data.user.uuid){
-      editLink = <Link to={"/app/p/" + this.props.data.uuid}><i className="fa fa-pencil"/></Link>
+    if (tree.get('user').uuid === this.props.data.user.uuid) {
+      editLink = <Link to={'/app/p/' + this.props.data.uuid}><i className='fa fa-pencil' /></Link>
     }
 
     return <div className='box'>
-          <article className='media'>
-            <div className='media-left'>
-              <figure className='image is-64x64'>
-                { this.props.data.siteIcon && <img src={this.props.data.siteIcon} alt='Image' />}
-              </figure>
-            </div>
-            <div className='media-content'>
-              <div className='content'>
-                <p>
-                  <strong>{this.props.data.siteName}</strong> <small>{this.props.data.title}</small>
-                  <br /><small></small>
-                  <nav className="level is-mobile">
-                    <div className="level-left">
-                        {moment(this.props.data.createdAt).format('LLLL')}
-                    </div>
-                    <div className="level-right">
-                      {postsTags}
-                    </div>
-                  </nav>
-                  <div className="card">
-                    <div className="card-content">
-                      <p className="subtitle">
-                        <Link className='highlight' to={"/u/" + this.props.data.user.screenName}>{"@" + this.props.data.user.screenName}</Link> learned in this site:
-                      </p>
-                      <p className="title" style={{marginTop: 20, marginBottom: 20}}>
-                        “{this.props.data.description}”
-                      </p>
-
-                    </div>
-                  </div>
-                  <br/>
-                  <div className={this.state.isToggleOn ? 'collapse-content' : 'show-content'} dangerouslySetInnerHTML={{__html: this.props.data.html}} />
-                </p>
-              </div>
-              <div>
-                <a className="button" onClick={() => this.handleClick()}>{this.state.isToggleOn ? 'More...' : 'Less...'}</a>
-              </div>
-              <nav className="level is-mobile">
-                <div className="level-left">
-
-                </div>
-                <div className="level-right">
-                  <a className="level-item">
-                    <span className="icon is-medium"><i className="fa fa-reply"></i></span>
-                  </a>
-                  <a className="level-item">
-                    <span className="icon is-medium"><i className="fa fa-heart"></i></span>
-                  </a>
-                  <a className="level-item">
-                    <span className="icon is-medium"><i className="fa fa-ellipsis-h"></i></span>
-                  </a>
-                  {editLink}
+      <article className='media'>
+        <div className='media-content'>
+          <div className='content'>
+            <p>
+              {this.props.data.siteIcon && (<figure className='image is-64x64' style={{float: 'left', margin: 10}}>
+                <img src={this.props.data.siteIcon} alt='Image' />
+              </figure>)}
+              <p style={{marginBottom: 0}}><strong>{this.props.data.title}</strong></p>
+              <p style={{marginBottom: 0}}><small>{this.props.data.siteName}</small></p>
+              <nav className='level is-mobile'>
+                <div className='level-left'>
+                  {moment(this.props.data.createdAt).format('LLLL')}
                 </div>
               </nav>
+              <div className='level-right'>
+                {postsTags}
+              </div>
+            </p>
+          </div>
+          <div>
+            <p className='subtitle'>
+              <Link className='highlight' to={'/u/' + this.props.data.user.screenName}>{'@' + this.props.data.user.screenName}</Link> learned in this site:
+            </p>
+            <p className='title' style={{marginTop: 20, marginBottom: 20}}>
+              “{this.props.data.description}”
+            </p>
+          </div>
+          <div className='card'>
+            <div className='card-content'>
+              <div className={this.state.isToggleOn ? 'collapse-content' : 'show-content'} dangerouslySetInnerHTML={{__html: this.props.data.html}} />
             </div>
-          </article>
+          </div>
+          <nav className='level is-mobile' style={{marginTop: 10}}>
+            <div className='level-left'>
+              <a className='button' onClick={() => this.handleClick()}>{this.state.isToggleOn ? 'Read...' : 'Hide...'}</a>
+            </div>
+            <div className='level-right'>
+              <a className='level-item'>
+                <span className='icon is-medium'><i className='fa fa-reply' /></span>
+              </a>
+              <a className='level-item'>
+                <span className='icon is-medium'><i className='fa fa-heart' /></span>
+              </a>
+              <a className='level-item'>
+                <span className='icon is-medium'><i className='fa fa-ellipsis-h' /></span>
+              </a>
+              {editLink}
+            </div>
+          </nav>
         </div>
+      </article>
+    </div>
   }
 }
 
